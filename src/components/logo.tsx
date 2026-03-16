c10cf15adde32d7f2fc3da31b63d7130df3364f1
Define Perspective <defineperspective.in@gmail.com>
2026-03-16 18:19:24 +0000

    replace it with https://drive.google.com/uc?export=view&id=16s6sucIsRneT

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn(className)}>
       <Image
-        src="/logo.png"
+        src="https://drive.google.com/uc?export=view&id=16s6sucIsRneTujL_vZE8ajIuYUmZ3cFq"
         alt="Craftsmac Labs Logo"
         width={180}
         height={35}
